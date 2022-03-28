c882b9592ccec4104bd503fda264c2d08f105c2e
Spasko Katsarski <patsokatsarski1@gmail.com>
2022-03-28 21:12:31 +0300

    More Exam Tasks Solved

---
+++ T02. Emoji Detector/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace T02._Emoji_Detector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string emojiPattern = @"(?<border>\:\:|\*\*)(?<emoji>[A-Z][a-z]{2,})\1";
+            string numberPattern = @"\d";
+
+            string input = Console.ReadLine();
+
+            Regex regex = new Regex(emojiPattern);
+            MatchCollection validEmojis = regex.Matches(input);
+
+            MatchCollection numbers = Regex.Matches(input, numberPattern);
+
+            BigInteger coolThreshold = 1;
+            foreach (Match number in numbers)
+            {
+                coolThreshold *= int.Parse(number.Value);
+            }
+
+            Console.WriteLine($"Cool threshold: {coolThreshold}");
+
+            // We have the cool threshold and now we have to check which emojis we should keep and then print.
+
+            Dictionary<string, string> coolEmojis = new Dictionary<string, string>();
+
+            int foundEmojisCount = 0;
+            foreach (Match emoji in validEmojis)
+            {
+                string currEmoji = emoji.Groups["emoji"].Value;
+                int currSum = 0;
+
+                foreach (char ch in currEmoji)
+                {
+                    currSum += ch;
+                }
+
+                if (currSum > coolThreshold)
+                {
+                    coolEmojis.Add(currEmoji, emoji.Groups["border"].Value);
+                }
+
+                foundEmojisCount++;
+            }
+
+            Console.WriteLine($"{foundEmojisCount} emojis found in the text. The cool ones are:");
+            foreach (KeyValuePair<string, string> emoji in coolEmojis)
+            {
+                Console.WriteLine($"{emoji.Value}{emoji.Key}{emoji.Value}");
+            }
+        }
+    }
+}
---
+++ T03. P!rates/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T03._P_rates
+{
+    class City
+    {
+        public City(int population, int gold)
+        {
+            this.Population = population;
+            this.Gold = gold;
+        }
+
+        public int Population { get; set; }
+
+        public int Gold { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, City> cities = new Dictionary<string, City>();
+
+            string input;
+            while ((input = Console.ReadLine()) != "Sail")
+            {
+                string[] cityInfo = input
+                    .Split("||", StringSplitOptions.RemoveEmptyEntries);
+
+                string cityName = cityInfo[0];
+                int population = int.Parse(cityInfo[1]);
+                int gold = int.Parse(cityInfo[2]);
+
+                if (!cities.ContainsKey(cityName))
+                {
+                    cities.Add(cityName, new City(0, 0));
+                }
+
+                cities[cityName].Population += population;
+                cities[cityName].Gold += gold;
+            }
+
+            string command;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] cmdArgs = command
+                    .Split("=>", StringSplitOptions.RemoveEmptyEntries);
+
+                string action = cmdArgs[0];
+                string cityName = cmdArgs[1];
+
+                if (action == "Plunder")
+                {
+                    int peopleKilled = int.Parse(cmdArgs[2]);
+                    int goldStolen = int.Parse(cmdArgs[3]);
+
+                    cities[cityName].Population -= peopleKilled;
+                    cities[cityName].Gold -= goldStolen;
+
+                    Console.WriteLine($"{cityName} plundered! {goldStolen} gold stolen, {peopleKilled} citizens killed.");
+
+                    if (IsCityDead(cities, cityName))
+                    {
+                        cities.Remove(cityName);
+                        Console.WriteLine($"{cityName} has been wiped off the map!");
+                    }
+                }
+                else if (action == "Prosper")
+                {
+                    int goldToAdd = int.Parse(cmdArgs[2]);
+
+                    if (goldToAdd < 0)
+                    {
+                        Console.WriteLine("Gold added cannot be a negative number!");
+                    }
+                    else
+                    {
+                        cities[cityName].Gold += goldToAdd;
+
+                        Console.WriteLine($"{goldToAdd} gold added to the city treasury. {cityName} now has {cities[cityName].Gold} gold.");
+                    }
+                }
+            }
+
+            if (cities.Count > 0)
+            {
+                Console.WriteLine($"Ahoy, Captain! There are {cities.Count} wealthy settlements to go to:");
+
+                foreach (KeyValuePair<string, City> city in cities)
+                {
+                    Console.WriteLine($"{city.Key} -> Population: {city.Value.Population} citizens, Gold: {city.Value.Gold} kg");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ahoy, Captain! All targets have been plundered and destroyed!");
+            }
+        }
+
+        static bool IsCityDead(Dictionary<string, City> cities, string cityName)
+        {
+            if (cities[cityName].Population <= 0 || cities[cityName].Gold <= 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
